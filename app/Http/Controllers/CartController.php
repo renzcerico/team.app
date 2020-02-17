6da21abcce316b243637f05d55bc69b7bd149dfc
Renz Martin C. Cerico <cerico.renzmartin@gmail.com>
2020-02-18 00:08:03 +0800

    cart api

---
+++ app/Http/Controllers/CartController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Cart;
+
+class CartController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $cart = new Cart();
+        $cart = $cart->all();
+
+        return $cart;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create(Request $request)
+    {
+        $cart = new Cart;
+        $cart->product_id = $request->productId;
+        $cart->quantity = $request->quantity;
+        $cart->save();
+
+        return $cart::latest()->first();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $cart = new Cart;
+        $cart = $cart->findOrFail($id);
+
+        return $cart;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $cart = new Cart;
+        $cart = $cart->findOrFail($id);
+        $cart->product_id = $request->productId;
+        $cart->quantity = $request->quantity;
+        $cart->save();
+
+        return $cart;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $cart = new Cart;
+        $cart = $cart->findorFail($id);
+        $cart->delete();
+
+        return $cart;
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/cart', 'CartController@index');
+Route::get('/cart/{id}', 'CartController@show');
+Route::post('/cart', 'CartController@create');
+Route::post('/cart/{id}', 'CartController@destroy');
+Route::post('/cart-update/{id}', 'CartController@update');
